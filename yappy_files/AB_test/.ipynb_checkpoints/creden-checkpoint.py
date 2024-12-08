f00c27fbcfa7d30bca32c833246f1e627b22f48e
Кемран Мехтиев <kemran@MacBook-Pro-Kemran.local>
2024-12-08 23:55:45 +0300

    Файл из yappy

---
+++ yappy_files/AB_test/.ipynb_checkpoints/creden-checkpoint.py
@@ -0,0 +1,13 @@
+postgres_secret = {
+    'db_name': 'product_x',
+    'user': 'product_x_ro',
+    'password': '9sNDZKJM5taKHfKZsoFvQMsMbtgSef',
+    'host': 'rc1a-yiz11yr42igms5a6.mdb.yandexcloud.net'
+}
+
+clickhouse_dwh_secret = {
+    'db_name': 'product_x',
+    'user': 'kmekhtiev',
+    'password': 'mGsFX7fblCQxgcL8CuU8',
+    'host': '10.129.11.136'
+}
